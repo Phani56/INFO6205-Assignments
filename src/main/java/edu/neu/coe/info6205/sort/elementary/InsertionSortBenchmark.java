9b0c0ce45987c5c4f176dea93eb43aff895d6271
Phani56 <gphani567@gmail.com>
2021-09-26 23:54:31 -0400

    added report and updated main benchmark code

--- src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSortBenchmark.java
+++ src/main/java/edu/neu/coe/info6205/sort/elementary/InsertionSortBenchmark.java
@@ -1,6 +1,5 @@
 package edu.neu.coe.info6205.sort.elementary;
 
-import edu.neu.coe.info6205.sort.*;
 import edu.neu.coe.info6205.util.*;
 
 import java.lang.reflect.Array;
@@ -10,21 +9,41 @@ import java.util.function.Supplier;
 
 public class InsertionSortBenchmark{
 
+
     public static void main(String[] args) {
         Random random = new Random();
         int n = 4000;
 
-        final Supplier<Integer[]> integersSupplier = () -> {
-            Integer[] result = (Integer[]) Array.newInstance(Integer.class, n);
-            for (int i = 0; i < n; i++) result[i] = random.nextInt();
-            return result;
-        };
+        for (int count=0; count<7; count++) {
+            int finalN = n;
 
-        final double runTime = new Benchmark_Timer<Integer[]>(
-                "InsertionSort Benchmark",
-                InsertionSort::sort
-        ).runFromSupplier(integersSupplier, 100);
-        System.out.println(runTime);
+            // LIST OF SUPPLIERS TO BE USED
+            final Supplier<Integer[]> randomIntegersSupplier = () -> {
+                Integer[] result = (Integer[]) Array.newInstance(Integer.class, finalN);
+                for (int i = 0; i < finalN; i++) result[i] = random.nextInt();
+                return result;
+            };
+
+            final Supplier<Integer[]> orderedIntegersSupplier = () -> {
+                Integer[] result = (Integer[]) Array.newInstance(Integer.class, finalN);
+                for (int i = 0; i < finalN; i++) result[i] = i;
+                return result;
+            };
+
+            final Supplier<Integer[]> reverseOrderedIntegersSupplier = () -> {
+                Integer[] result = (Integer[]) Array.newInstance(Integer.class, finalN);
+                for (int i = 0; i < finalN; i++) result[i] = finalN - i;
+                return result;
+            };
+
+            final double runTime = new Benchmark_Timer<Integer[]>(
+                    "InsertionSort Benchmark",
+                    (xs) -> Arrays.copyOf(xs, xs.length),
+                    InsertionSort::sort,
+                    null
+            ).runFromSupplier(reverseOrderedIntegersSupplier, 100); // REPLACE SUPPLIER AND TEST FOR EACH TYPE
+            System.out.println("Runtime for n value " + n + " is :" +runTime);
+            n = n*2;
+        }
     }
-
 }
